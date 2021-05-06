15d12c832ae838628a0147a7e4c8c2caa1d58f75
Marcos Fujihashi <marcosfujihashi@gmail.com>
2021-05-05 21:23:45 -0300

    added test class

---
+++ src/test/java/ChallengeTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class ChallengeTests {
+
+    @Test
+    public void test() {
+
+        //TODO: implement test scenarios
+        //Challenge.getSmallestNegativeBalance();
+
+    }
+}
